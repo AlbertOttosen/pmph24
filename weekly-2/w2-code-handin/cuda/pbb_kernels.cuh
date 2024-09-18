8a1a8e6d16fdc116863a6ad186b7f4dfb6e08c7f
Albert <albertarvad@gmail.com>
2024-09-18 14:52:51 +0200

    weekly2

--- weekly-2/w2-code-handin/cuda/pbb_kernels.cuh
+++ weekly-2/w2-code-handin/cuda/pbb_kernels.cuh
@@ -235,7 +235,10 @@ scanIncBlock(volatile typename OP::RedElTp* ptr, const unsigned int idx) {
     //   the first warp. This works because
     //   warp size = 32, and
     //   max block size = 32^2 = 1024
-    if (lane == (WARP-1)) { ptr[warpid] = OP::remVolatile(ptr[idx]); }
+    if (lane == (WARP-1)) { 
+        __syncthreads();
+        ptr[warpid] = OP::remVolatile(ptr[idx]); 
+    }
     __syncthreads();
 
     // 3. scan again the first warp.
@@ -246,7 +249,6 @@ scanIncBlock(volatile typename OP::RedElTp* ptr, const unsigned int idx) {
     if (warpid > 0) {
         res = OP::apply(ptr[warpid-1], res);
     }
-    __syncthreads();
 
     return res;
 }
