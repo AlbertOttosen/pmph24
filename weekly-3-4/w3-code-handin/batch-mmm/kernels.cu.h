94ddab2b92259e729caf329272a7dcc8d254cb58
Albert <albertarvad@gmail.com>
2024-10-07 14:51:49 +0200

    w34

--- weekly-3-4/w3-code-handin/batch-mmm/kernels.cu.h
+++ weekly-3-4/w3-code-handin/batch-mmm/kernels.cu.h
@@ -106,7 +106,8 @@ void bmmmTiledKer ( ElTp* A,      ElTp* B, char* X_tr,   ElTp* Y
       ElTp ab = A[j1 * N + q] * B[q * K + j2];
 
       // Load X_tr into shared memory (only for threads within bounds)
-      char x = (flat_thid < T && i + flat_thid < M) ? X_tr[q * M + i + flat_thid] : 0; // Bounds check
+      int i_flat = i + flat_thid
+      char x = (flat_thid < T && i_flat < M) ? X_tr[q * M + i_flat] : 0; // Bounds check
       Xsh_tr[flat_thid] = x;
 
       // Synchronize to ensure all threads have loaded their part of X_tr
